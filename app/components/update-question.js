65c950256872a808f448a1dca8bbd0e6219bd3e8
Ben Logue <benjamin.logue73@gmail.com>
2016-08-19 11:07:04 -0700

    implement update for questions.

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    update(question) {
+      var params = {
+        topic: this.get('topic'),
+        author: this.get('author'),
+        date: new Date(Date.now()).toString(),
+        content: this.get('content'),
+      };
+      this.sendAction('update', question, params);
+    }
+  }
 });
