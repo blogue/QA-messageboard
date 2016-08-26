8ec4845d1d9669bae1983216771708d0b84f255d
Ben Logue <benjamin.logue73@gmail.com>
2016-08-26 11:49:08 -0700

    sort answers by vote count on question page.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,6 +4,8 @@ export default Ember.Component.extend({
   answerCount: Ember.computed('question.answers.length', function(){
     return this.get('question.answers.length');
   }),
+  sortBy: ['votes:desc'],
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
   actions: {
     upVote: function(answer) {
       answer.set('votes', answer.get('votes') + 1);
